9dd1b5d68a0f771b4d8f8b1fbf570e7b320573ce
Spike <renziyang1999@outlook.com>
2023-11-26 23:02:53 +1300

    increase batch size :sparkles:

--- src/main/kotlin/com/persons/finder/domain/services/SaveDataServiceImpl.kt
+++ src/main/kotlin/com/persons/finder/domain/services/SaveDataServiceImpl.kt
@@ -4,48 +4,68 @@ import com.persons.finder.data.Location
 import com.persons.finder.data.Person
 import com.persons.finder.respository.LocationRepository
 import com.persons.finder.respository.PersonRepository
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import javax.transaction.TransactionManager
 
 /**
  * Asynchronously store big data for testing
  */
+
+const val size = 1000
 @Service
 class SaveDataServiceImpl(
     private val personRepository: PersonRepository,
-    private val locationsRepository: LocationRepository
+    private val locationsRepository: LocationRepository,
 ) : SaveDataService {
     @Async
+    @Transactional
     override fun saveMillionData() {
 
         val locationList: MutableList<Location> = mutableListOf()
         for (i in 3..1000000) {
             val location = Location(null, i.toLong(), 9999.9999, 99999.9999)
             locationList.add(location);
+            if(locationList.size == 1000) {
+                locationsRepository.saveAll(locationList)
+                locationsRepository.flush()
+                locationList.clear()
+            }
         }
-        locationsRepository.saveAll(locationList)
 
     }
 
     @Async
+    @Transactional
     override fun saveTenMillionData() {
 
         val personList: MutableList<Person> = mutableListOf()
         for (i in 1..10000000) {
             val person = Person(null, "xu")
-            personList.add(person);
+            personList.add(person)
+            if (personList.size == 1000) {
+                personRepository.saveAll(personList)
+                personRepository.flush()
+                personList.clear()
+            }
+
         }
-        personRepository.saveAll(personList);
     }
 
     @Async
+    @Transactional
     override fun saveHundredMillionData() {
         val personList: MutableList<Person> = mutableListOf()
         for (i in 1..100000000) {
             val person = Person(null, "xu")
             personList.add(person);
+            if (personList.size == 1000) {
+                personRepository.saveAll(personList)
+                personRepository.flush()
+                personList.clear()
+            }
         }
-        personRepository.saveAll(personList);
     }
 }
